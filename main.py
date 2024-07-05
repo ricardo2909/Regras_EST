73beb2352fff1e7f17c722d11cb62d59b50f672b
ricardo2909 <ricardoi@al.insper.edu.br>
2024-07-05 16:08:02 -0300

    inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Regras Gerais - EST')
+
+st.write("")
+st.write("")
+st.write("")
+with st.expander("Como Selecionamos Ativos"):
+    st.write("explicacao...")
+
+with st.expander("Como Escolhemos Fundos"):
+    st.write("explicacao...")
